7c18f7a947d14e024c3de2fa6a9f87ff1ae2da76
S-M124 <enemy.good1@yahoo.com>
2024-12-02 01:11:34 +0100

    Add files via upload

---
+++ Project16/JS/node.js
@@ -0,0 +1,4 @@
+let amountOfMinutes = 20;
+const secondsConverter = 60;
+let convertedIntoSeconds = amountOfMinutes * secondsConverter;
+console.log("The current minutes", amountOfMinutes, "converted to seconds is", convertedIntoSeconds, "seconds");
